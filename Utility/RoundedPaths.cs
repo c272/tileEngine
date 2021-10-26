541d911cf9a46933add3537728df76e34ef3abde
C272 <realrussianmanconfirmed@gmail.com>
2021-10-26 12:51:10 +0100

    Add rounding to nodes.

--- Utility/RoundedPaths.cs
+++ Utility/RoundedPaths.cs
@@ -22,7 +22,38 @@ namespace easyCase.Utility
         {
             //todo
             var path = new GraphicsPath();
-            path.AddRectangle(rect);
+            path.StartFigure();
+
+            //Add the center rectangle.
+            float diameter = radius * 2;
+            path.AddRectangle(new RectangleF(rect.Left, rect.Top + radius, rect.Width, rect.Height - diameter));
+            
+            //Add the top depending on rounding.
+            if (roundTop)
+            {
+                path.AddEllipse(rect.Left, rect.Top, diameter, diameter);
+                path.AddEllipse(rect.Right - diameter, rect.Top, diameter, diameter);
+                path.AddRectangle(new RectangleF(rect.Left + radius, rect.Top, rect.Width - diameter, radius));
+            }
+            else
+            {
+                path.AddRectangle(new RectangleF(rect.Left, rect.Top, rect.Width, radius));
+            }
+
+            //Add the bottom depending on rounding.
+            if (roundBottom)
+            {
+                path.AddEllipse(rect.Left, rect.Bottom - diameter, diameter, diameter);
+                path.AddEllipse(rect.Right - diameter, rect.Bottom - diameter, diameter, diameter);
+                path.AddRectangle(new RectangleF(rect.Left + radius, rect.Bottom - radius, rect.Width - diameter, radius));
+            }
+            else
+            {
+                path.AddRectangle(new RectangleF(rect.Left, rect.Bottom - radius, rect.Width, radius));
+            }
+
+            //Set to winding to prevent colour over-draw.
+            path.FillMode = FillMode.Winding;
             return path;
         }
     }
